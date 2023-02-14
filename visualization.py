4b4fcdcbb2aae7f9b4334fa9e5dbb6a32a32f875
yesheysamdrup <89333430+yesheysamdrup@users.noreply.github.com>
2023-02-14 14:28:57 +0000

    Delete visualization.py

--- visualization.py
+++
@@ -1,9 +0,0 @@
-import pandas
-import matplotlib.pyplot as plt
-from sqlalchemy import create_engine
- 
-engine = create_engine("postgresql:///?User=postgres&;Password=postgres&Database=esakor&Server=127.0.0.1&Port=5432")
-df = pandas.read_sql("SELECT adescr, count(cthram) FROM vthramdistrictwise", engine)
-print(df)
-df.plot(kind="bar", x="District", y="Total Thrams")
-plt.show()
